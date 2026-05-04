1001f56df281172c6880b392d784246be452a191
Alok Dev <mk.alok806@gmail.com>
2026-05-04 13:30:42 +0530

    Add files via upload

---
+++ Java Learning Journey/02-Variable/variables.java
@@ -0,0 +1,43 @@
+public class variables{
+public static void main(String[] args) {
+    
+    // Steps for creating a variable :
+    // 1. Declaration
+    // 2. Assignment
+
+    // Interger :
+
+    int age = 19;
+    int year = 2025;
+    int quantity = 5;
+
+
+    // Double of Float :
+
+    double price = 19.99;
+    double cgpa = 8.35;
+    double temperature = -12.5;
+
+
+    // Character :
+
+    char grade = 'A';
+    char symbol = '!';
+    char currency ='$';
+
+
+    // Boolean : 
+
+    boolean isStudent = true;
+    boolean forSale = false;
+    boolean isOnline = true;
+
+    // String : 
+
+    String name = "AlokDev";
+    String country = "India";
+    String email = "xyz@gmail.com";
+
+
+}
+}
