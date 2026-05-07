13a23ba357f8728ee764d0709d376bf7d8f7a67a
Alok Dev <mk.alok806@gmail.com>
2026-05-07 13:16:21 +0530

    Add files via upload

---
+++ Java Learning Journey/05-Arithmetic/AugmentedArithmeticOperator.java
@@ -0,0 +1,18 @@
+public class AugmentedArithmeticOperator {
+    public static void main(String[] args) {
+
+//        Augmented Arithmetic Operators
+
+        int x = 10;
+        int y = 20;
+
+        x +=y;  // x = x + y
+        x -=y;  // x = x - y
+        x *=y;  // x = x * y
+        x /=y;  // x = x / y
+        x %=y;  // x = x % y
+
+        System.out.println(x);
+
+    }
+}
