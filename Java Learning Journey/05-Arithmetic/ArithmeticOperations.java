4d17cd5d59d51ca9eac8c0bc3cb4b8717169c5b1
Alok Dev <mk.alok806@gmail.com>
2026-05-07 13:16:01 +0530

    Add files via upload

---
+++ Java Learning Journey/05-Arithmetic/ArithmeticOperations.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+//        Arithmetic Operators
+
+        int x = 10;
+        int y = 20;
+        int z;
+
+        z=x+y;
+        z=x-y;
+        z=x*y;
+        z=x/y;
+        z=x%y;
+
+        System.out.println(z);
+
+    }
+}
