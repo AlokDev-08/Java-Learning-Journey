0d3a0625f4e38961a81e4632aec917a1eebb54c9
Alok Dev <mk.alok806@gmail.com>
2026-05-08 21:51:45 +0530

    Add files via upload

---
+++ Java Learning Journey/09-Math Class/Examples.java
@@ -0,0 +1,18 @@
+public class Examples {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI); // Returns the exact value of pi
+        System.out.println(Math.E); //  Returns the exact value of exponent
+
+        double result;
+
+        result = Math.pow(2 , 5); // Returns the value of 2^5 = 30.0
+        result = Math.abs(-5);   //  Return the absolute (positive) value
+        result = Math.sqrt(9);  //   Return the square root of 9
+        result = Math.round(2.56); // Return the round off value
+        result = Math.ceil(2.78); // Return the round value of a decimal number upward to the nearest mathematical integer
+        result = Math.floor(3.99); // Return the round value of a decimal number downward to the nearest mathematical integer
+        result = Math.max(10,20); // Return the maximum value
+        result = Math.min(10,20); // Return the minimum value
+    }
+}
