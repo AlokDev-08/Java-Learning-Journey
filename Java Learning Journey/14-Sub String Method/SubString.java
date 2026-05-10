9566bf08adb7e6f71b15c69419aaf9fb8493f3e3
Alok Dev <mk.alok806@gmail.com>
2026-05-10 19:21:07 +0530

    Add files via upload

---
+++ Java Learning Journey/14-Sub String Method/SubString.java
@@ -0,0 +1,8 @@
+public class SubString {
+    public static void main(String[] args) {
+
+//        .substring() = A method used to extract a portion of a string.substring( start, end )
+
+        
+    }
+}
