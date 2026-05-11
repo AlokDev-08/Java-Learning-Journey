19040d3f4fc0ae9e188c2dd448a3c1caee17b297
Alok Dev <mk.alok806@gmail.com>
2026-05-11 18:16:16 +0530

    Add files via upload

---
+++ Java Learning Journey/16-Ternary Operator/Score.java
@@ -0,0 +1,13 @@
+public class Score {
+    public static void main(String[] args) {
+
+//        ternary operator (?) = Return 1 or 2 values if a condition is true
+
+//        variable = (condition) ? ifTrue : ifFalse;
+
+        int score = 85;
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL"   ;
+        System.out.println(passOrFail);
+    }
+}
