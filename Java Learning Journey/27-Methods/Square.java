982ff914d88be5c725d91fc3b2a7a526b77ecfd0
Alok Dev <mk.alok806@gmail.com>
2026-05-13 22:09:18 +0530

    Add files via upload

---
+++ Java Learning Journey/27-Methods/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    public static void main(String[]args){
+
+//        Methods  = a block of reusable code that is executed when called ()
+
+        double number = square(3);
+            System.out.println(number);
+    }
+    static double square(double number){
+        return number*number;
+    }
+}
